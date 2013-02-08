5612114dcacc19e8d9fa9982b124752f90d32678
Nick DeNardis <nick.denardis@gmail.com>
2013-02-07 19:51:59 -0500

    Started to give the web page a view page

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Localize a web page
 =============
 
 Takes a URL, downloads all of its assets and zips it for download and archiving.
+
+Setup
+-------------
+1. Download the source
+2. Run "rake db:migrate"
+3. Enjoy
--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,4 +2,26 @@ class WebsitesController < ApplicationController
   def index
   	@website = Website.new
   end
+
+  def create 
+  	# Setup the data from the form
+  	@website = Website.new params[:website]
+
+  	# Save the website to the DB
+  	if @website.save
+  		redirect_to website_path(@website), :flash => { :notice => "Successfully localized #{@website.url}" }
+  	else
+  		redirect_to :back, :flash => { :error => "Something went wrong localizing your site." }
+  	end
+  end
+
+  def show
+    # Find the site in the DB
+    @website = Website.find(params[:id])
+
+    # Ensure the site is owned by this user
+    if !@website
+      redirect_to :back, :flash => { :error => 'Not a valid website.' }
+    end
+  end
 end
